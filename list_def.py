086f087297a25ab459bf1faf2490fa5224b4d8f5
0-0 <39878411+reighpuy@users.noreply.github.com>
2020-05-17 01:16:28 +0700

    Add files via upload

---
+++ list_def.py
@@ -0,0 +1,111 @@
+from important import *
+from module import *
+from setup_args import *
+
+settings = livejson.File('setting.json', True, False, 4)
+bool_dict = {
+    True: ['Yes', 'Aktif', 'Sukses', 'Open', 'On'],
+    False: ['No', 'Tidak Aktif', 'Gagal', 'Close', 'Off']
+}
+
+def restartProgram():
+    print ('Pesan Sistem: *Memulai Ulang Program.')
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+def timeChange(secs):
+    mins, secs = divmod(secs,60)
+    hours, mins = divmod(mins,60)
+    days, hours = divmod(hours,24)
+    weeks, days = divmod(days,7)
+    months, weeks = divmod(weeks,4)
+    text = ""
+    if months != 0: text += "%02d Bulan" % (months)
+    if weeks != 0: text += " %02d Minggu" % (weeks)
+    if days != 0: text += " %02d Hari" % (days)
+    if hours !=  0: text +=  " %02d Jam" % (hours)
+    if mins != 0: text += " %02d Menit" % (mins)
+    if secs != 0: text += " %02d Detik" % (secs)
+    if text[0] == " ":
+        text = text[1:]
+    return text
+
+def logError(error, write=True):
+    errid = str(random.randint(100, 999))
+    filee = open('tmp/errors/%s.txt'%errid, 'w') if write else None
+    if args.traceback: traceback.print_tb(error.__traceback__)
+    if write:
+        traceback.print_tb(error.__traceback__, file=filee)
+        filee.close()
+        with open('errorLog.txt', 'a') as e:
+            e.write('\n%s : %s'%(errid, str(error)))
+    print ('++ Error : {error}'.format(error=error))
+
+def command(text):
+    pesan = text.lower()
+    if settings['setKey']['status']:
+        if pesan.startswith(settings['setKey']['key']):
+            cmd = pesan.replace(settings['setKey']['key'],'')
+        else:
+            cmd = 'Undefined command'
+    else:
+        cmd = text.lower()
+    return cmd
+
+def genImageB64(path):
+    with open(path, 'rb') as img_file:
+        encode_str = img_file.read()
+        b64img = base64.b64encode(encode_str)
+        return b64img.decode('utf-8')
+
+def genUrlB64(url):
+    return base64.b64encode(url.encode('utf-8')).decode('utf-8')
+
+def removeCmd(text, key=''):
+    if key == '':
+        setKey = '' if not settings['setKey']['status'] else settings['setKey']['key']
+    else:
+        setKey = key
+    text_ = text[len(setKey):]
+    sep = text_.split(' ')
+    return text_[len(sep[0] + ' '):]
+
+def multiCommand(cmd, list_cmd=[]):
+    if True in [cmd.startswith(c) for c in list_cmd]:
+        return True
+    else:
+        return False
+
+def replaceAll(text, dic):
+    try:
+        rep_this = dic.items()
+    except:
+        rep_this = dic.iteritems()
+    for i, j in rep_this:
+        text = text.replace(i, j)
+    return text
+
+def parsingRes(res):
+    result = ''
+    textt = res.split('\n')
+    for text in textt:
+        if True not in [text.startswith(s) for s in ['╭', '├', '│', '╰']]:
+            result += '\n│ ' + text
+        else:
+            if text == textt[0]:
+                result += text
+            else:
+                result += '\n' + text
+    return result
+
+def sendMention(to, mid, firstmessage='', lastmessage=''):
+    arrData = ""
+    text = "%s " %(str(firstmessage))
+    arr = []
+    mention = "@zeroxyuuki "
+    slen = str(len(text))
+    elen = str(len(text) + len(mention) - 1)
+    arrData = {'S':slen, 'E':elen, 'M':mid}
+    arr.append(arrData)
+    text += mention + str(lastmessage)
+    client.sendMessage(to, text, {'MENTION': str('{"MENTIONEES":' + json.dumps(arr) + '}')}, 0)
